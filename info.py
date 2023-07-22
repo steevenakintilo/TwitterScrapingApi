dff5b4fbf0963d9ee869dda1652ee85133dd83d0
steevenakintilo <72024403+steevenakintilo@users.noreply.github.com>
2023-07-22 10:13:02 +0200

    [ADD] I can retrieve user tweet and rt on 2 seperate function comment are comming soon

---
+++ info.py
@@ -0,0 +1,91 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+from src.util.num import *
+from src.util.list import *
+
+import time
+import traceback
+
+def _get_user_info(S,user,info):
+    try:
+        if info == "media":
+            S.driver.get("https://twitter.com/"+user+"/media")
+            info = "primaryColumn"
+        elif info == "like":
+            S.driver.get("https://twitter.com/"+user+"/likes")
+            info = "primaryColumn"
+        else:   
+            S.driver.get("https://twitter.com/"+user)
+        
+        element = WebDriverWait(S.driver, 15).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '[data-testid="'+info+'"]')))
+                
+        elem = S.driver.find_element(By.CSS_SELECTOR, '[data-testid="'+info+'"]')
+        
+        elem = elem.text
+        return (elem)
+    except Exception as e:
+        print(info , " error")
+
+def get_user_info(S,user):
+    user_info = {"username":"",
+    "bio":"",
+    "birthdate":"",
+    "tweet_count":0,
+    "follower_count":0,
+    "following_count":0}
+    try:
+        S.driver.get("https://twitter.com/"+user)
+        element = WebDriverWait(S.driver, 15).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '[data-testid="UserName"]')))
+        
+        username = S.driver.find_element(By.CSS_SELECTOR, '[data-testid="UserName"]').text
+        bio = S.driver.find_element(By.CSS_SELECTOR, '[data-testid="UserDescription"]').text
+        try:
+            birthdate = S.driver.find_element(By.CSS_SELECTOR, '[data-testid="UserBirthdate"]').text
+        except:
+            birthdate = "None"
+        tweet_count = parse_number(str(str(S.driver.find_element(By.CSS_SELECTOR, '[data-testid="primaryColumn"]').text.split("\n")[1]).split("\n")[0]).split(" ")[0])
+        follower_count = parse_number(get_elem_from_list(S.driver.find_element(By.CSS_SELECTOR, '[data-testid="primaryColumn"]').text.split("\n"),"Followers").split(" ")[0])
+        try:
+            following_count = parse_number(get_elem_from_list_special(S.driver.find_element(By.CSS_SELECTOR, '[data-testid="primaryColumn"]').text.split("\n"),"Following").split(" ")[0])
+        except:
+            following_count = parse_number(get_elem_from_list(S.driver.find_element(By.CSS_SELECTOR, '[data-testid="primaryColumn"]').text.split("\n"),"Following").split(" ")[0])
+        #print(str(S.driver.find_element(By.CSS_SELECTOR, '[data-testid="primaryColumn"]').text.split("\n")))
+        #print(get_elem_from_list_special(S.driver.find_element(By.CSS_SELECTOR, '[data-testid="primaryColumn"]').text.split("\n"),"Following").split(" ")[0])
+        #follower_count = 0
+        user_info = {"username":username,
+        "bio":bio,
+        "birthdate":birthdate,
+        "tweet_count":tweet_count,
+        "follower_count":follower_count,
+        "following_count":following_count}
+        return (user_info)
+    except Exception as e:
+        print("User info error")
+        traceback.print_exc()
+        return(user_info)
+
+def get_user_number_of_media(S,user):
+    try:
+        nb_of_media = _get_user_info(S,user,"media")
+        nb_of_media.split("\n")[1]
+        nb_of_media = nb_of_media.split("Photos & videos")
+        nb_of_media = str(nb_of_media[0]).split("\n")
+        return(parse_number(nb_of_media[1]))
+    except:
+        return (0)
+            
+def get_user_number_of_like(S,user):
+    try:
+        nb_of_like = _get_user_info(S,user,"like")
+        nb_of_like.split("\n")[1]
+        nb_of_like = nb_of_like.split("Likes")
+        nb_of_like = str(nb_of_like[0]).split("\n")
+        return(parse_number(nb_of_like[1]))
+    except:
+        return(0)
---
+++ tweet.py
@@ -0,0 +1,232 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+from src.util.num import *
+from src.util.list import *
+from src.user.info import get_user_info
+from src.tweet.comment import is_tweet_a_comment
+
+import time
+
+import traceback
+
+def get_list_of_user_tweet_url(S,account,nb_of_tweet_to_search):
+    try:
+        account = account.replace("@","")
+        nb = 0
+        nb_of_tweet = int(get_user_info(S,account)["tweet_count"])
+        if nb_of_tweet_to_search < 3000 and nb_of_tweet_to_search >= nb_of_tweet:
+            nb_of_tweet_to_search = nb_of_tweet_to_search - 1
+        S.driver.get("https://twitter.com/"+account)
+        run  = True
+        list_of_tweet_url = []
+        selenium_data = []
+        p = '"'
+        flop = 0
+        if nb_of_tweet_to_search > 3000:
+            nb_of_tweet_to_search = 3000
+        while run:
+            element = WebDriverWait(S.driver, 15).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '[data-testid="tweet"]')))
+            tweets_info = S.driver.find_elements(By.CSS_SELECTOR, '[data-testid="tweet"]')
+            last_tweet = tweets_info[len(tweets_info) - 1]
+            for tweet_info in tweets_info:
+                if len(list_of_tweet_url) >= nb_of_tweet_to_search:
+                    run = False
+                if tweet_info not in selenium_data:
+                    try:
+                        lower_data = str(tweet_info.get_property('outerHTML')).lower()
+                        
+                        splinter = "href="+p+"/"+account+"/status"
+                        splinter = splinter.replace("\\","/")
+                        
+                        lower_data = lower_data.split(splinter)
+                        lower_data = str(lower_data[1])
+                        lower_data = lower_data.split(" ")
+                        tweet_id = lower_data[0].replace("/","").replace(p,"")
+                        tweet_link = "https://twitter.com/" + account + "/status/" + tweet_id
+                                            
+                        list_of_tweet_url.append(tweet_link)
+                        selenium_data.append(tweet_info)
+                        S.driver.execute_script("arguments[0].scrollIntoView();", last_tweet)
+                        time.sleep(0.05)
+                    except:
+                        try:
+                            lower_data = str(tweet_info.get_property('outerHTML')).lower()
+                            splinter = "href=" + p + "/"
+                            lower_data = lower_data.split(splinter)
+                            user = lower_data[5]
+                            user = user.split(p)
+                            tweet_stuff = user[0]
+                            tweet_link = "https://twitter.com/" + tweet_stuff
+                            if user_to_check.lower() == account.lower():
+                                list_of_tweet_url.append(tweet_link)
+                            selenium_data.append(tweet_info)
+                            S.driver.execute_script("arguments[0].scrollIntoView();", last_tweet)
+                            time.sleep(0.05)
+                        except Exception as e:    
+                            flop = flop + 1
+                            print("flop " , flop)
+                            #print("error " , error)
+                            time.sleep(0.05)
+        return(list_of_tweet_url)
+        print("Listing tweet end")
+    except Exception as e:
+        print("Error feetching " + account + " tweet")
+        traceback.print_exc()
+        return([])
+
+
+def get_list_of_user_rt_url(S,account,nb_of_tweet_to_search):
+    try:
+        account = account.replace("@","")
+        nb = 0
+        nb_of_tweet = int(get_user_info(S,account)["tweet_count"])
+        if nb_of_tweet_to_search < 3000 and nb_of_tweet_to_search >= nb_of_tweet:
+            nb_of_tweet_to_search = nb_of_tweet_to_search - 1
+        S.driver.get("https://twitter.com/"+account)
+        run  = True
+        list_of_tweet_url = []
+        list_of_rt_url = []
+        selenium_data = []
+        p = '"'
+        flop = 0
+        if nb_of_tweet_to_search > 3000:
+            nb_of_tweet_to_search = 3000
+        while run:
+            element = WebDriverWait(S.driver, 15).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '[data-testid="tweet"]')))
+            tweets_info = S.driver.find_elements(By.CSS_SELECTOR, '[data-testid="tweet"]')
+            last_tweet = tweets_info[len(tweets_info) - 1]
+            for tweet_info in tweets_info:
+                if len(list_of_rt_url) >= nb_of_tweet_to_search:
+                    run = False
+                if len(list_of_tweet_url) >= 3000:
+                    run = False
+                if tweet_info not in selenium_data:
+                    try:
+                        lower_data = str(tweet_info.get_property('outerHTML')).lower()
+                        
+                        splinter = "href="+p+"/"+account+"/status"
+                        splinter = splinter.replace("\\","/")
+                        
+                        lower_data = lower_data.split(splinter)
+                        lower_data = str(lower_data[1])
+                        lower_data = lower_data.split(" ")
+                        tweet_id = lower_data[0].replace("/","").replace(p,"")
+                        tweet_link = "https://twitter.com/" + account + "/status/" + tweet_id
+                        list_of_tweet_url.append(tweet_link)               
+                        selenium_data.append(tweet_info)
+                        S.driver.execute_script("arguments[0].scrollIntoView();", last_tweet)
+                        time.sleep(0.025)
+                    except:
+                        try:
+                            lower_data = str(tweet_info.get_property('outerHTML')).lower()
+                            splinter = "href=" + p + "/"
+                            lower_data = lower_data.split(splinter)
+                            user = lower_data[5]
+                            user = user.split(p)
+                            tweet_stuff = user[0]
+                            tweet_link = "https://twitter.com/" + tweet_stuff
+                            user_to_check = tweet_stuff.split("/")[0]
+                            if user_to_check.lower() != account.lower():
+                                list_of_rt_url.append(tweet_link)
+                                list_of_tweet_url.append(tweet_link)
+                            selenium_data.append(tweet_info)
+                            S.driver.execute_script("arguments[0].scrollIntoView();", last_tweet)
+                            time.sleep(0.025)
+                        except Exception as e:    
+                            flop = flop + 1
+                            print("flop " , flop)
+                            #print("error " , error)
+                            time.sleep(0.025)
+        return(list_of_rt_url)
+        print("Listing tweet end")
+    except Exception as e:
+        print("Error feetching " + account + " tweet")
+        traceback.print_exc()
+        return([])
+
+
+def get_list_of_user_comment_url(S,account,nb_of_tweet_to_search):
+    try:
+        account = account.replace("@","")
+        nb = 0
+        nb_of_tweet = int(get_user_info(S,account)["tweet_count"])
+        if nb_of_tweet_to_search < 3000 and nb_of_tweet_to_search >= nb_of_tweet:
+            nb_of_tweet_to_search = nb_of_tweet_to_search - 1
+        S.driver.get("https://twitter.com/"+account+"/with_replies")
+                        
+        run  = True
+        list_of_tweet_url = []
+        list_of_comment_url = []
+        selenium_data = []
+        p = '"'
+        flop = 0
+        if nb_of_tweet_to_search > 3000:
+            nb_of_tweet_to_search = 3000
+        while run:
+            element = WebDriverWait(S.driver, 15).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '[data-testid="tweet"]')))
+            
+            tweets_info = S.driver.find_elements(By.CSS_SELECTOR, '[data-testid="tweet"]')
+            last_tweet = tweets_info[len(tweets_info) - 1]
+            
+            for tweet_info in tweets_info:
+                if len(list_of_comment_url) >= nb_of_tweet_to_search:
+                    run = False
+                if len(list_of_tweet_url) >= 3000:
+                    run = False
+                if tweet_info not in selenium_data:
+                    try:
+                        lower_data = str(tweet_info.get_property('outerHTML')).lower()
+                        print("acacacacacacacacacacacacaca")
+                        time.sleep(100000)
+        
+                        splinter = "href="+p+"/"+account+"/status"
+                        splinter = splinter.replace("\\","/")
+                        
+                        lower_data = lower_data.split(splinter)
+                        lower_data = str(lower_data[1])
+                        lower_data = lower_data.split(" ")
+                        tweet_id = lower_data[0].replace("/","").replace(p,"")
+                        tweet_link = "https://twitter.com/" + account + "/status/" + tweet_id
+                        if is_tweet_a_comment(S,tweet_link) == True:
+                            list_of_comment_url.append(tweet_link)       
+                            list_of_tweet_url.append(tweet_link)
+                        selenium_data.append(tweet_info)    
+                        
+                        S.driver.execute_script("arguments[0].scrollIntoView();", last_tweet)
+                        time.sleep(2)
+                    except:
+                        try:
+                            lower_data = str(tweet_info.get_property('outerHTML')).lower()
+                            splinter = "href=" + p + "/"
+                            lower_data = lower_data.split(splinter)
+                            user = lower_data[5]
+                            user = user.split(p)
+                            tweet_stuff = user[0]
+                            tweet_link = "https://twitter.com/" + tweet_stuff
+                            if user_to_check.lower() == account.lower():
+                                if is_tweet_a_comment(S,tweet_link) == True:
+                                    list_of_tweet_url.append(tweet_link)
+                                    list_of_comment_url.append(tweet_link)
+                            selenium_data.append(tweet_info)
+                            S.driver.execute_script("arguments[0].scrollIntoView();", last_tweet)
+                            time.sleep(2)
+                        except Exception as e:    
+                            flop = flop + 1
+                            print("flop " , flop)
+                            #print("error " , error)
+                            time.sleep(2)
+        print("infity" * 10)
+        time.sleep(10000)
+        return(list_of_comment_url)
+        print("Listing tweet end")
+    except Exception as e:
+        print("Error feetching " + account + " tweet")
+        #traceback.print_exc()
+        return([])
